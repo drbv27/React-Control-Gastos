6df3b033cbd631a30e8fdf2f94a457cda8ee2c96
Diego <drbv27@gmail.com>
2022-01-24 07:51:48 -0500

    creating the budget form

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import NuevoPresupuesto from "./NuevoPresupuesto";
-const Header = () => {
+const Header = ({ presupuesto, setPresupuesto }) => {
   return (
     <header>
       <h1>Planificador de Gastos</h1>
-      <NuevoPresupuesto />
+      <NuevoPresupuesto
+        presupuesto={presupuesto}
+        setPresupuesto={setPresupuesto}
+      />
     </header>
   );
 };
