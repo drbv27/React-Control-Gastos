c2f02bdff8ef4f089c31f6feb7ee27237b1a302b
Diego <drbv27@gmail.com>
2022-01-25 16:58:51 -0500

    adiing the button to new gasto

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Header from "./components/Header";
+import IconoNuevoGasto from "./img/nuevo-gasto.svg";
 
 function App() {
   const [presupuesto, setPresupuesto] = useState(0);
@@ -12,6 +13,11 @@ function App() {
         isValidPresupuesto={isValidPresupuesto}
         setIsValidPresupuesto={setIsValidPresupuesto}
       />
+      {isValidPresupuesto && (
+        <div className="nuevo-gasto">
+          <img src={IconoNuevoGasto} alt="iconogasto" />
+        </div>
+      )}
     </>
   );
 }
