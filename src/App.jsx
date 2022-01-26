985864df53ded4f3788cef1bf6053e899bd6a52d
Diego <drbv27@gmail.com>
2022-01-26 12:05:40 -0500

    creating the modal showing and hiding

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
 import Header from "./components/Header";
+import Modal from "./components/Modal";
 import IconoNuevoGasto from "./img/nuevo-gasto.svg";
 
 function App() {
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+  const [modal, setModal] = useState(false);
+
+  const handleNuevoGasto = () => {
+    setModal(true);
+  };
   return (
     <>
       <Header
@@ -15,9 +21,14 @@ function App() {
       />
       {isValidPresupuesto && (
         <div className="nuevo-gasto">
-          <img src={IconoNuevoGasto} alt="iconogasto" />
+          <img
+            src={IconoNuevoGasto}
+            alt="iconogasto"
+            onClick={handleNuevoGasto}
+          />
         </div>
       )}
+      {modal && <Modal setModal={setModal} />}
     </>
   );
 }
