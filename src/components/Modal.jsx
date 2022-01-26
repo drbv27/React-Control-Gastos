49c8eea5e00b6af61b9a20665d7096fb7c8a695d
Diego <drbv27@gmail.com>
2022-01-26 12:33:24 -0500

    adding the form

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -14,6 +14,35 @@ const Modal = ({ setModal, animarModal, setanimarModal }) => {
       </div>
       <form className={`formulario ${animarModal ? "animar" : "cerrar"}`}>
         <legend>Nuevo Gasto</legend>
+        <div className="campo">
+          <label htmlFor="nombre">Nombre Gasto</label>
+          <input
+            type="text"
+            placeholder="añade el nombre del gasto"
+            id="nombre"
+          />
+        </div>
+        <div className="campo">
+          <label htmlFor="cantidad">Cantidad</label>
+          <input
+            type="number"
+            placeholder="añade la cantidad del gasto"
+            id="cantidad"
+          />
+        </div>
+        <div className="campo">
+          <label htmlFor="categoria">Categoría</label>
+          <select name="" id="categoria">
+            <option value="">-- Seleccione --</option>
+            <option value="ahorro">Ahorro</option>
+            <option value="comida">Comida</option>
+            <option value="gastos">Gastos Varios</option>
+            <option value="diversion">Tiempo Libre</option>
+            <option value="salud">Salud</option>
+            <option value="suscripciones">Suscripciones</option>
+          </select>
+        </div>
+        <input type="submit" value="Añadir Gasto" />
       </form>
     </div>
   );
